2f00a8ff7e90443f79223aa342e6209aad29fa8d
EC2 Default User <ec2-user@ip-172-31-6-36.ec2.internal>
2020-10-09 02:04:50 +0000

    [Finish]検索機能

--- bookers2/app/models/book.rb
+++ bookers2/app/models/book.rb
@@ -1,4 +1,4 @@
- class Book < ApplicationRecord
+class Book < ApplicationRecord
 	belongs_to :user
 	has_many :book_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
@@ -8,4 +8,16 @@
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
- end
+
+def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+end
